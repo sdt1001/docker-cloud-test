a92df069d7969213792425ed52c3612b8bd326eb
Steve <Steve>
2017-04-17 19:59:18 -0400

    added two tests to unh698_test

--- unh698.py
+++ unh698.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 def hello_world():
     return 'UNH698 Website!'
 
+def test_link_to_my_page(self):
+	rv = self.app.get('/')
+	# Search the page contents for the link to my topic page
+	# Replace xxxxxxxxxxxx with text you'd expect to see on my main page that links to my subpage
+	assert b'xxxxxxxxxxxx' in rv.data
+
+def test_my_topic(self):
+	# Replace '/' with the page path I want to make
+	rv = self.app.get('/')
+	# Replace UNH698 Website with the text I expect to see on my topic page
+	assert b'UNH698 Website' in rv.data
+
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=8080)
 
--- unh698_test.py
+++ unh698_test.py
@@ -17,6 +17,18 @@ class FlaskrTestCase(unittest.TestCase):
         # Chech that the page contians the desired phrase
         assert b'UNH698 Website' in rv.data
 
+    def test_link_to_my_page(self):
+        rv = self.app.get('/')
+        # Search the page contents for the link to my topic page
+        # Replace xxxxxxxxxxxx with text you'd expect to see on my main page that links to my subpage
+        assert b'xxxxxxxxxxxx' in rv.data
+
+    def test_my_topic(self):
+        # Replace '/' with the page path I want to make
+        rv = self.app.get('/')
+        # Replace UNH698 Website with the text I expect to see on my topic page
+        assert b'UNH698 Website' in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
