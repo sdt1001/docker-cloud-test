b0707680a66a83eb5dfb145b06bd4e15daec2a85
Steve <Steve>
2017-04-26 20:38:07 -0400

    added topic subpage to webpage

--- unh698.py
+++ unh698.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'UNH698 Website!'
+def mainPage():
+    return render_template('main.html')
+
+@app.route('/topic_page')
+def topicPage():
+	return render_template('topic.html')
 
 if __name__ == "__main__":
 	app.run(debug=True,host='0.0.0.0', port=8080)
